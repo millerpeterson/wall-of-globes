abd092d93fd5434d91c602db154b26957abc39d0
Miller Peterson <miller.peterson@gmail.com>
2022-02-07 23:22:08 -0500

    Add coordinate mapping fn

--- internal/geom/geom.go
+++ internal/geom/geom.go
@@ -1,5 +1,10 @@
 package geom
 
+type Vec struct {
+	X int
+	Y int
+}
+
 type Rect struct {
 	Width  int
 	Height int
@@ -9,8 +14,8 @@ func (r Rect) AspectRatio() float32 {
 	return float32(r.Width) / float32(r.Height)
 }
 
-// MaxInnerFit Return the largest (i.e. max area) rectangle with
-// the same aspect ratio of `inner` that fits inside `outer`.
+// MaxInnerFit - Return the largest (i.e. max area) rectangle with the same
+// aspect ratio of `inner` that fits inside `outer`.
 func MaxInnerFit(inner Rect, outer Rect) Rect {
 	innerAspect := inner.AspectRatio()
 	// Try fitting to width
@@ -21,4 +26,15 @@ func MaxInnerFit(inner Rect, outer Rect) Rect {
 	// Fit to height
 	heightFitWidth := int(float32(outer.Height) * innerAspect)
 	return Rect{heightFitWidth, outer.Height}
+}
+
+// MapRectPos - Return the position that `srcPos` from `src` would map to in
+// `target`.
+func MapRectPos(src Rect, target Rect, srcPos Vec) Vec {
+	widthRatio := float32(target.Width) / float32(src.Width)
+	heightRatio := float32(target.Height) / float32(src.Height)
+	return Vec{
+		X: int(float32(srcPos.X) * widthRatio),
+		Y: int(float32(srcPos.Y) * heightRatio),
+	}
 }
--- internal/geom/geom_test.go
+++ internal/geom/geom_test.go
@@ -3,30 +3,68 @@ package geom
 import "testing"
 
 func TestMaxInnerFit(t *testing.T) {
-	// Same aspect ratio
-	inner := Rect{300, 200}
-	outer := Rect{600, 400}
-	expected := outer
-	max := MaxInnerFit(inner, outer)
-	if max != expected {
-		t.Errorf("Expect %v, got %v", expected, max)
-	}
+	t.Run("Same aspect ratio", func(t *testing.T) {
+		inner := Rect{300, 200}
+		outer := Rect{600, 400}
+		expected := outer
+		max := MaxInnerFit(inner, outer)
+		if max != expected {
+			t.Errorf("Expected %v, got %v", expected, max)
+		}
+	})
 
-	// Inner wider ratio than outer
-	inner = Rect{400, 200}
-	outer = Rect{600, 400}
-	expected = Rect{600, 300}
-	max = MaxInnerFit(inner, outer)
-	if max != expected {
-		t.Errorf("Expect %v, got %v", expected, max)
-	}
+	t.Run("Inner wider ratio than outer", func(t *testing.T) {
+		inner := Rect{400, 200}
+		outer := Rect{600, 400}
+		expected := Rect{600, 300}
+		max := MaxInnerFit(inner, outer)
+		if max != expected {
+			t.Errorf("Expected %v, got %v", expected, max)
+		}
+	})
 
-	// Inner narrower ratio than outer
-	inner = Rect{200, 200}
-	outer = Rect{600, 400}
-	expected = Rect{400, 400}
-	max = MaxInnerFit(inner, outer)
-	if max != expected {
-		t.Errorf("Expect %v, got %v", expected, max)
-	}
+	t.Run("Inner narrower ratio than outer", func(t *testing.T) {
+		inner := Rect{200, 200}
+		outer := Rect{600, 400}
+		expected := Rect{400, 400}
+		max := MaxInnerFit(inner, outer)
+		if max != expected {
+			t.Errorf("Expected %v, got %v", expected, max)
+		}
+	})
+}
+
+func TestMapRectPos(t *testing.T) {
+	t.Run("src smaller than target", func(t *testing.T) {
+		src := Rect{300, 200}
+		target := Rect{600, 400}
+		srcPos := Vec{150, 100}
+		expected := Vec{300, 200}
+		targetPos := MapRectPos(src, target, srcPos)
+		if targetPos != expected {
+			t.Errorf("Expected %v, got %v", expected, targetPos)
+		}
+	})
+
+	t.Run("src larger than target", func(t *testing.T) {
+		src := Rect{900, 600}
+		target := Rect{600, 400}
+		srcPos := Vec{150, 100}
+		expected := Vec{100, 66}
+		targetPos := MapRectPos(src, target, srcPos)
+		if targetPos != expected {
+			t.Errorf("Expected %v, got %v", expected, targetPos)
+		}
+	})
+
+	t.Run("src same size as target", func(t *testing.T) {
+		src := Rect{600, 550}
+		target := Rect{600, 550}
+		srcPos := Vec{23, 45}
+		expected := Vec{23, 45}
+		targetPos := MapRectPos(src, target, srcPos)
+		if targetPos != expected {
+			t.Errorf("Expected %v, got %v", expected, targetPos)
+		}
+	})
 }
