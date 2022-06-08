1aa74aadc383001ee057d6bbcabf6750f741c050
Miller Peterson <miller.peterson@gmail.com>
2022-06-07 23:12:30 -0400

    Clean up random changes I made while debugging

--- internal/wall/wall.go
+++ internal/wall/wall.go
@@ -2,7 +2,6 @@ package wall
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/millerpeterson/wall-of-globes/internal/tiling"
 	"io/ioutil"
 	"os"
@@ -19,30 +18,25 @@ type Wall struct {
 func Load(fp string) (Wall, error) {
 	wl := Wall{}
 	jsonFile, err := os.Open(fp)
-	fmt.Println("1")
 	if err != nil {
 		return wl, err
 	}
 	defer func(jsonFile *os.File) {
-		fmt.Println("2")
 		err := jsonFile.Close()
 		if err != nil {
 			panic(err)
 		}
 	}(jsonFile)
 
-	fmt.Println("3")
 	bytes, err := ioutil.ReadAll(jsonFile)
 	if err != nil {
 		return wl, err
 	}
 
-	fmt.Println("4")
 	err = json.Unmarshal(bytes, &wl)
 	if err != nil {
 		return wl, err
 	}
 
-	fmt.Println("5")
 	return wl, nil
 }
--- internal/wall/wall_test.go
+++ internal/wall/wall_test.go
@@ -1,47 +1,41 @@
 package wall
 
-//
-//import (
-//	"fmt"
-//	"github.com/millerpeterson/wall-of-globes/internal/geom"
-//	"github.com/millerpeterson/wall-of-globes/internal/tiling"
-//	"reflect"
-//	"testing"
-//)
-//
-//func TestLoad(t *testing.T) {
-//	expected := Wall{
-//		Tln: tiling.Tiling{
-//			"one": tiling.Tile{
-//				Offset: geom.Vec{10, 10},
-//				Rect:   geom.Rect{300, 200},
-//			},
-//			"two": tiling.Tile{
-//				Offset: geom.Vec{50, 70},
-//				Rect:   geom.Rect{600, 400},
-//			},
-//			"three": tiling.Tile{
-//				Offset: geom.Vec{250, 270},
-//				Rect:   geom.Rect{900, 600},
-//			},
-//		},
-//		ServerMap: map[string]string{
-//			"one":   "192.168.0.1",
-//			"two":   "192.168.0.2",
-//			"three": "192.168.0.3",
-//		},
-//	}
-//	loaded, err := Load("test_wall.json")
-//
-//	fmt.Println("10")
-//	if err != nil {
-//		t.Errorf("Failed to load wall data: %v", err)
-//	}
-//
-//	fmt.Println("11")
-//	if !reflect.DeepEqual(expected, loaded) {
-//		t.Errorf("Expected %v, got %v", expected, loaded)
-//	}
-//
-//	fmt.Println("12")
-//}
+import (
+	"github.com/millerpeterson/wall-of-globes/internal/geom"
+	"github.com/millerpeterson/wall-of-globes/internal/tiling"
+	"reflect"
+	"testing"
+)
+
+func TestLoad(t *testing.T) {
+	expected := Wall{
+		Tln: tiling.Tiling{
+			"one": tiling.Tile{
+				Offset: geom.Vec{10, 10},
+				Rect:   geom.Rect{300, 200},
+			},
+			"two": tiling.Tile{
+				Offset: geom.Vec{50, 70},
+				Rect:   geom.Rect{600, 400},
+			},
+			"three": tiling.Tile{
+				Offset: geom.Vec{250, 270},
+				Rect:   geom.Rect{900, 600},
+			},
+		},
+		ServerMap: map[string]string{
+			"one":   "192.168.0.1",
+			"two":   "192.168.0.2",
+			"three": "192.168.0.3",
+		},
+	}
+	loaded, err := Load("test_wall.json")
+
+	if err != nil {
+		t.Errorf("Failed to load wall data: %v", err)
+	}
+
+	if !reflect.DeepEqual(expected, loaded) {
+		t.Errorf("Expected %v, got %v", expected, loaded)
+	}
+}
